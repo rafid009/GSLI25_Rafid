a857ef4df51186bf8b3be66c077d7d3fa2e26a92
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2025-09-13 09:53:39 -0700

    Dataset NACSE added 1-5

--- Code/generate_missing_values_nacse.py
+++ Code/generate_missing_values_nacse.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import os
 
 data = pd.read_csv("Data/nacse/nacse_norm.csv")
 
@@ -10,5 +11,6 @@ for i in range(mask.shape[0]):
     index = np.random.randint(0, temp.shape[0])
     temp[index] = 0
     mask[i] = temp.reshape(-1)
-
+if not os.path.isdir("Data/nacse/mask"):
+    os.makedirs("Data/nacse/mask")
 np.savetxt("Data/nacse/mask/nacse_mask.csv", mask, fmt="%d", delimiter=",")
