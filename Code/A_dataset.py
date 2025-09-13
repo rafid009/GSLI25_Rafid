fce68d796b82798124e90d2cdbccb42a8a97345c
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2025-09-13 15:54:38 -0700

    METRLA and PEMSBAY added 1-8

--- Code/A_dataset.py
+++ Code/A_dataset.py
@@ -127,8 +127,13 @@ class METRLA_DATASET(Dataset):
 
     def __getitem__(self, index):
         data_res = self.data[index * self.configs.seq_len: (index+1) * self.configs.seq_len]
-        print(f"index: {index}, start: {index * self.configs.seq_len}, end: {(index+1) * self.configs.seq_len}")
+        # print(f"index: {index}, start: {index * self.configs.seq_len}, end: {(index+1) * self.configs.seq_len}")
         mask_res = self.mask[index * self.configs.seq_len: (index+1) * self.configs.seq_len]
+        if mask_res.shape[0] < data_res.shape[0]:
+            res = data_res.shape[0] - mask_res.shape[0]
+            for i in range(res):
+                ones = torch.ones((data_res.shape[1], data_res.shape[2]))
+                mask_res = torch.cat([mask_res, ones], axis=0)
         observed_tp = np.arange(self.configs.seq_len)
         gt_mask_res = self.gt_mask[index * self.configs.seq_len: (index+1) * self.configs.seq_len]
 
@@ -136,7 +141,7 @@ class METRLA_DATASET(Dataset):
         mask_res = torch.from_numpy(mask_res).float()
         observed_tp = torch.from_numpy(observed_tp).float()
         gt_mask_res = torch.from_numpy(gt_mask_res).float()
-        print(f"index: {index}, data res: {data_res.shape}, mask: {mask_res.shape}, gt mask: {gt_mask_res.shape}, obs tp: {observed_tp.shape}")
+        # print(f"index: {index}, data res: {data_res.shape}, mask: {mask_res.shape}, gt mask: {gt_mask_res.shape}, obs tp: {observed_tp.shape}")
         return data_res, observed_tp, mask_res, gt_mask_res
 
 def get_metrla_dataset(configs):
