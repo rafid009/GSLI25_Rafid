afa112479205fae76146cbda71fef92846a415c3
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2025-09-13 09:51:43 -0700

    Dataset NACSE added 1-4

--- Code/generate_missing_values_nacse.py
+++ Code/generate_missing_values_nacse.py
@@ -9,6 +9,6 @@ for i in range(mask.shape[0]):
     temp = mask[i].copy().reshape(-1, 2)
     index = np.random.randint(0, temp.shape[0])
     temp[index] = 0
-    mask[i] = temp.rehsape(-1)
+    mask[i] = temp.reshape(-1)
 
 np.savetxt("Data/nacse/mask/nacse_mask.csv", mask, fmt="%d", delimiter=",")
